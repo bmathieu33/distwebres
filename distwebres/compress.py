45cbbd80a2c8f983a3ebf1403289bd76793c6182
Bertrand Mathieu <bert.mathieu@gmail.com>
2011-07-01 12:01:52 +0200

    don't fail if no compressor defined: this allow to run the command against any package, for standard build/install workflow

--- distwebres/compress.py
+++ distwebres/compress.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from distutils.cmd import Command
+from distutils import log
 
 class compress(Command):
     # Brief (40-50 characters) description of the command
@@ -14,9 +15,11 @@ class compress(Command):
 
     def finalize_options (self):
         self.compressors = self.compressors.split()
-        if not self.compressors:
-            raise ValueError, "No compressor defined"
 
     def run (self):
+        if not self.compressors:
+            log.info('No rule for JS or CSS compression, skipping.')
+            return
+
         for command in self.compressors:
             self.run_command(command)
